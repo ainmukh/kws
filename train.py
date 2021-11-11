52323b65474682c07838e9028bbe4d3d9baeb10c
ainmukh <aynurmukh@gmail.com>
2021-11-12 00:11:14 +0300

    chng train

--- train.py
+++ train.py
@@ -19,7 +19,6 @@ def train():
 
     labeled_data.sample(3)
 
-
     # get dataframes
     indexes = torch.randperm(len(labeled_data))
     train_indexes = indexes[:int(len(labeled_data) * 0.8)]
@@ -28,17 +27,14 @@ def train():
     train_df = labeled_data.iloc[train_indexes].reset_index(drop=True)
     val_df = labeled_data.iloc[val_indexes].reset_index(drop=True)
 
-
     # Sample is a dict of utt, word and label
     transform_tr = AugsCreation()
     train_set = TrainDataset(df=train_df, kw=TaskConfig.keyword, transform=transform_tr)
     val_set = TrainDataset(df=val_df, kw=TaskConfig.keyword)
 
-
     # samplers
     train_sampler = get_sampler(train_set.df['label'].values)
     val_sampler = get_sampler(val_set.df['label'].values)
-
 
     # Here we are obliged to use shuffle=False because of our sampler with randomness inside.
     train_loader = DataLoader(train_set, batch_size=TaskConfig.batch_size,
@@ -51,14 +47,11 @@ def train():
                             sampler=val_sampler,
                             num_workers=2, pin_memory=True)
 
-
     # melspecs
     melspec_train = LogMelspec(is_train=True, config=TaskConfig)
     melspec_val = LogMelspec(is_train=False, config=TaskConfig)
 
-
     history = defaultdict(list)
-
 
     CRNN_model = CRNN(TaskConfig)
     attn_layer = AttnMech(TaskConfig)
@@ -69,7 +62,6 @@ def train():
 
     opt = torch.optim.Adam(full_model.parameters(),
                            lr=TaskConfig.learning_rate, weight_decay=TaskConfig.weight_decay)
-
 
     for n in range(TaskConfig.num_epochs):
         train_epoch(full_model, opt, train_loader,
